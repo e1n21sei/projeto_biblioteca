04028d32541058e20c50f124528b666f0c9bc789
Maria Mariana Mandu <maria.mandu6172273@serra.pe.senac.br>
2026-08-22 02:03:55 +0000

    classe livro (chatinha)

--- src/Livro.java
+++ src/Livro.java
@@ -1,3 +1,57 @@
-public class Livro {
-    
+public class Livro implements Disponibilidade {
+
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponibilidade;
+
+     public void setTitulo(String titulo){
+    this.titulo = titulo;
+   }
+
+    public String getTitulo(){
+    return titulo;
+    }
+
+     public void setAutor(String autor){
+    this.autor = autor;
+   }
+
+    public String getAutor(){
+    return autor;
+   }
+
+    public void setanoPublicacao(int anoPublicacao){
+    this.anoPublicacao = anoPublicacao;
+   }
+
+    public int getanoPublicacao(){
+    return anoPublicacao;
+   }
+
+   public boolean isDisponibilidade(){
+    return disponibilidade;
+   }
+
+   public void setDisponibilidade(boolean disponibilidade){
+    this.disponibilidade = disponibilidade;
+   }
+
+   public Livro(String titulo, String autor, int anoPublicacao, boolean disponibilidade){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anoPublicacao = anoPublicacao;
+    this.disponibilidade = disponibilidade;
+   }
+
+    @Override
+    public void emprestar(){
+        disponibilidade = false;
+    }
+
+    @Override
+    public void devolver(){
+        disponibilidade = true;
+    }
+
 }
