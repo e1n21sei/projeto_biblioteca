770d7a38e8e19f4614a23ad5caf6fec8224f0166
Maria Mariana Mandu <maria.mandu6172273@serra.pe.senac.br>
2026-08-22 04:24:44 +0000

    Banco de dados (pós sono)

--- src/BancoDados.java
+++ src/BancoDados.java
@@ -7,6 +7,21 @@ public class BancoDados {
         livros = new ArrayList<>();
         usuarios = new ArrayList<>();
 
+       Livro livro1 = new Livro("Dom Casmurro", "Machado de Assis", 1899, true);
+        adicionarLivro(livro1);
+
+        Livro livro2 = new Livro("A Hora da Estrela", "Clarice Lispector", 1977, true);
+        adicionarLivro(livro2);
+
+        Livro livro3 = new Livro("Vidas Secas", "Graciliano Ramos", 1938, true);
+        adicionarLivro(livro3);
+
+        Livro livro4 = new Livro("Capitães da Areia", "Jorge Amado", 1937, true);
+        adicionarLivro(livro4);
+
+        Livro livro5 = new Livro("O Cortiço", "Aluísio Azevedo", 1890, true);
+        adicionarLivro(livro5);
+
     }
     public void adicionarLivro(Livro livro){
         livros.add(livro);
@@ -15,4 +30,5 @@ public class BancoDados {
     public void adicionarUsuario(Usuario usuario){
         usuarios.add(usuario);
     }
+
 }
