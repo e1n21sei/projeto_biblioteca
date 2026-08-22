b5e53b56fc9276f3f01020c2456eec80b1cf5afb
Maria Mariana Mandu <maria.mandu6172273@serra.pe.senac.br>
2026-08-22 01:06:10 +0000

    aluno e usuario

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,3 +1,31 @@
-public class Aluno {
-    
+public class Aluno extends Usuario {
+   private String curso;
+   private String turma;
+
+    public void setCurso(String curso){
+    this.curso = curso;
+   }
+
+    public String getCurso(){
+    return curso;
+   }
+
+   public void setTurma(String turma){
+    this.curso = turma;
+   }
+
+    public String getTurma(){
+    return turma;
+   }
+
+   public Aluno(String nome,String cpf, String email, String curso, String turma){
+    super(nome,cpf,email);
+    this.curso = curso;
+    this.turma = turma;
+   }
+
+    @Override
+   public void listarInformacoes(){
+
+   } 
 }
--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,22 @@
-public class Funcionario {
-    
+public class Funcionario extends Usuario {
+     private String cargo;
+
+    public void setCargo(String cargo){
+    this.cargo = cargo;
+   }
+
+    public String getCargo(){
+    return cargo;
+   }
+
+   public Funcionario(String nome,String cpf, String email, String cargo){
+    super(nome,cpf,email);
+    this.cargo = cargo;
+   }
+
+    @Override
+   public void listarInformacoes(){
+
+   } 
+
 }
