684390dba5cbbd77eec0439e44884907b8b9ef7a
Maria Mariana Mandu <maria.mandu6172273@serra.pe.senac.br>
2026-08-30 19:04:31 +0000

    switch (Acaba pelo amor de Deus)

--- src/Menu.java
+++ src/Menu.java
@@ -105,4 +105,56 @@ public class Menu{
         }
         System.err.println("Usuario Cadastrado com sucesso!");
     }
+    public static void exibirMenu() {
+    int opcao;
+
+    do {
+        System.out.println("\n========== *SISTEMA DA BIBLIOTECA* ==========");
+        System.out.println("1 - Cadastrar Livro");
+        System.out.println("2 - Listar Livros");
+        System.out.println("3 - Cadastrar Usuário");
+        System.out.println("4 - Listar Usuários");
+        System.out.println("5 - Pegar Livro");
+        System.out.println("6 - Devolver Livro");
+        System.out.println("7 - Sair");
+        System.out.print("Escolha uma opção: ");
+
+        opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1:
+                cadastrarLivro();
+                break;
+
+            case 2:
+                BancoDados.listarLivros();
+                break;
+
+            case 3:
+                cadastrarUsuario();
+                break;
+
+            case 4:
+                BancoDados.listarUsuarios();
+                break;
+
+            case 5:
+                realizarEmprestimo();
+                break;
+
+            case 6:
+                devolverLivro();
+                break;
+
+            case 7:
+                System.out.println("Saindo do sistema...");
+                break;
+
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+    } while (opcao != 7);
+}
 }
